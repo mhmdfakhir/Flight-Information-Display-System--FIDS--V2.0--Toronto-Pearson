89c340a89cc3199e3493218890246e6e80d279e7
codeproduction <7468@cch.edu.pk>
2023-06-15 15:13:34 +0300

    Final basic setup. Added flipping styling.

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-// const { response } = require("express")
+// const { table } = require("console")
 
 const tableBody = document.getElementById('table-body')
 
@@ -6,9 +6,43 @@ const getFlight = () => {
     fetch('http://localhost:8000/flights')
         .then(response => response.json())
         .then(flights => {
-            console.log(flights)
+            populateTable(flights)
         })
         .catch(err => console.log(err))
 }
-
 getFlight()
+
+const populateTable = (flights) => {
+    // console.log(flights)
+    for (let i = 0; i < 6; i++) {
+        const tableRow = document.createElement('tr')
+        const tableIcon = document.createElement('td')
+        tableIcon.textContent = "✈"
+        tableRow.append(tableIcon)
+
+        const flightDetails = {
+            time: flights.departures[i].movement.scheduledTime.local || 'N/A',
+            destination: flights.departures[i].movement.airport.name || 'N/A',
+            flight: flights.departures[i].number || 'N/A',
+            gate: flights.departures[i].movement.gate || 'N/A',
+            remarks: flights.departures[i].status || 'N/A',
+        }
+
+        for (const flightDetail in flightDetails) {
+            const tableCell = document.createElement('td')
+            const word = Array.from(flightDetails[flightDetail])
+
+            for (const [index, letter] of word.entries()) {
+                const letterElement = document.createElement('div')
+
+                setTimeout(() => {
+                    letterElement.classList.add('flip')
+                    letterElement.textContent = letter
+                    tableCell.append(letterElement)
+                }, 100 * index)
+            }
+            tableRow.append(tableCell)
+        }
+        tableBody.append(tableRow)
+    }
+}
--- server.js
+++ server.js
@@ -1,16 +1,62 @@
-const PORT = 8000
-const axios = require('axios').default
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-app.use(cors())
+// const PORT = 8000
+// const axios = require('axios').default
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
+// require('dotenv').config()
+// app.use(cors())
 
+
+// app.get('/flights', async (req, res) => {
+//     const options = {
+//         method: 'GET',
+//         url: 'https://aerodatabox.p.rapidapi.com/flights/airports/iata/YYZ/2023-06-14T08:00/2023-06-14T20:00',
+//         params: {
+//             withLeg: 'false',
+//             direction: 'Departure',
+//             withCancelled: 'true',
+//             withCodeshared: 'false',
+//             withCargo: 'false',
+//             withPrivate: 'false',
+//             withLocation: 'false'
+//         },
+//         headers: {
+//             'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+//             'X-RapidAPI-Host': 'aerodatabox.p.rapidapi.com'
+//         }
+//     }
+
+//     try {
+//         const response = await axios.request(options);
+//         console.log(response.data)
+//         res.json(response.data)
+//     } catch (error) {
+//         console.error(error)
+//     }
+// })
+
+
+
+// app.listen(PORT, () => console.log('Running on PORT' + PORT))
+
+const PORT = 8000;
+const axios = require('axios').default;
+const express = require('express');
+const moment = require('moment-timezone');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+app.use(cors());
 
 app.get('/flights', async (req, res) => {
+    const torontoTimezone = 'America/Toronto';
+    const currentDate = moment().tz(torontoTimezone); // Get the current date and time in Toronto timezone
+    const fromTime = currentDate.toISOString(); // Convert to ISO format
+    const toTime = currentDate.add(12, 'hours').toISOString(); // Add 12 hours
+
     const options = {
         method: 'GET',
-        url: 'https://aerodatabox.p.rapidapi.com/flights/airports/iata/YYZ/2023-06-14T08:00/2023-06-14T20:00',
+        url: `https://aerodatabox.p.rapidapi.com/flights/airports/iata/YYZ/${fromTime}/${toTime}`,
         params: {
             withLeg: 'false',
             direction: 'Departure',
@@ -24,17 +70,15 @@ app.get('/flights', async (req, res) => {
             'X-RapidAPI-Key': process.env.RAPID_API_KEY,
             'X-RapidAPI-Host': 'aerodatabox.p.rapidapi.com'
         }
-    }
+    };
 
     try {
         const response = await axios.request(options);
-        console.log(response.data)
-        res.json(response.data)
+        console.log(response.data);
+        res.json(response.data);
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
-})
+});
 
-
-
-app.listen(PORT, () => console.log('Running on PORT' + PORT))
+app.listen(PORT, () => console.log('Running on PORT ' + PORT));
